ce881617966f8ccd6f6058ebc48207bfdba47189
Jake Ferguson <jferguson15@my.wctc.edu>
2026-04-27 10:09:39 -0500

    build: Category.cs String length parameter

--- Model/Category.cs
+++ Model/Category.cs
@@ -6,6 +6,7 @@ namespace NorthwindConsole.Model;
 
 public partial class Category
 {
+  [StringLength(50)]
   public int CategoryId { get; set; }
   [Required(ErrorMessage = "Category Name is required.")]
   public string CategoryName { get; set; } = null!;
